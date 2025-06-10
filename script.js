a36bc69a37f6221ad16bda9994e7454597f34093
Jozef Kudrna <jojo25011990@gmail.com>
2025-06-10 22:39:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const headingPrimary = document.getElementById("heading-primary");
+
+const typed = new Typed(headingPrimary, {
+	strings: ["Typemaster keyboard", "Mechanical. Masterpiece."].reverse(),
+	typeSpeed: 50,
+});
